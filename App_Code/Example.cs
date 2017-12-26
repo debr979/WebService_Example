96370f7f75860981f67991df80a83c021af1e8d1
Chou KuanCheng <debr979@gmail.com>
2017-12-26 10:43:55 +0800

    webservice_example

---
+++ App_Code/Example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Example 的摘要描述
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請取消註解下列一行。
+// [System.Web.Script.Services.ScriptService]
+public class Example : System.Web.Services.WebService
+{
+
+    public Example()
+    {
+
+        //如果使用設計的元件，請取消註解下列一行
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public  string WebService_Example(string ex,string am)
+    {
+        if (ex !="" && am!="")
+        {
+
+            return "Success";
+        }
+        else {
+
+            return "fail";
+        }
+
+    }
+
+
+
+
+
+}
